865dc9f317f3dc1a8bc6e26ef7a9a022943f94b6
loick.ntambwe <loickntb@gmail.com>
2023-04-18 01:42:15 +0200

    test

---
+++ src/test/mainTest.java
@@ -0,0 +1,20 @@
+package src.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @Test
+    void eqTabChar() {
+    }
+
+    @Test
+    void eqStringTabChar() {
+    }
+
+    @Test
+    void motTrouve() {
+    }
+}
